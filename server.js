c5a06f904655e77422e74f59a02ec4efb6b91745
Gourav Khunger <gouravkhunger18@gmail.com>
2021-08-16 20:37:10 +0530

    fix CORS issue

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 // import libraries
-const express = require('express');
+const express = require("express");
 const app = express();
-const jwt = require('express-jwt');
-const jwksRsa = require('jwks-rsa');
-const jwtAuthz = require('express-jwt-authz');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const axios = require('axios');
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const jwtAuthz = require("express-jwt-authz");
+const bodyParser = require("body-parser");
+const axios = require("axios");
 
 // configure .env variables
-require('dotenv').config();
+require("dotenv").config();
 
 if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
-  throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file';
+  throw "Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file";
 }
 
 // The access token to access the Auth0 Management API
@@ -35,8 +34,8 @@ function getAccessToken() {
   // axios request to get the access token
   axios.post(`https://${process.env.AUTH0_DOMAIN}/oauth/token`, body, {
     headers: {
-      // Overwrite Axios's automatically set Content-Type
-      'Content-Type': 'application/json'
+      // Overwrite Axios"s automatically set Content-Type
+      "Content-Type": "application/json"
     }
   }).then(function (response) {
     // get the access token
@@ -49,7 +48,6 @@ function getAccessToken() {
     setTimeout(getAccessToken, (refreshAfter * 1000));
   }).catch(function (error) {
     accessToken = "";
-    console.error(error)
   });
 }
 
@@ -57,7 +55,15 @@ function getAccessToken() {
 getAccessToken();
 
 // Enable CORS
-app.use(cors());
+app.use(function(req, res, next){
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requestes-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+  next();
+});
 
 // Create middleware for checking the JWT
 const checkJwt = jwt({
@@ -66,13 +72,13 @@ const checkJwt = jwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: 'https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json'
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
   }),
 
   // Validate the audience and the issuer.
   audience: process.env.API_AUDIENCE,
-  issuer: 'https://${process.env.AUTH0_DOMAIN}/',
-  algorithms: ['RS256']
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ["RS256"]
 });
 
 // Enable the use of request body parsing middleware
@@ -81,20 +87,28 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// route to get the user data from Auth0 Management API
-app.get('/user/:id', checkJwt, function (req, res) {
+// API home, just to test if it"s working fine
+app.get("/", function(req, res){
+  res.status(200).send({"message":"API is functional."})
+});
 
-  // if accessToken is null
-  if(accessToken==='') {
+// route to get the user data from Auth0 Management API
+app.get("/user/:id", checkJwt, jwtAuthz(["read:userdata"]), function (req, res) {
+
+  // if accessToken is empty, get a new access token
+  // else process the request
+  if(accessToken==="") {
 
     getAccessToken();
 
   } else {
 
     var options = {
-      method: 'GET',
+      method: "GET",
       url: `https://${process.env.AUTH0_DOMAIN}/api/v2/users/${req.params.id}`,
-      headers: {authorization: `Bearer ${accessToken}`}
+      headers: {
+        "Authorization": `Bearer ${accessToken}`,
+      }
     };
     
     // send request
@@ -104,12 +118,12 @@ app.get('/user/:id', checkJwt, function (req, res) {
     }).catch(function (error) {
       // send error message and log it
       res.status(200).send({"message":`${error.message}`});
-      console.error(`${error.message}`);
     });
-
+    
   }
 });
 
-// launch the API Server at localhost:8080
-app.listen(8080);
-console.log('Listening on http://localhost:8080');
+// launch the API Server
+app.listen(8080, () => {
+  console.log("listening");
+});
